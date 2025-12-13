eaed191e123ffaa319484b8c825534941df9441c
AYAN ANWAR <152398480+AyanAnwar78@users.noreply.github.com>
2025-12-14 00:06:08 +0530

    Update CORS origin to include admin frontend

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ let app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["https://luxycart-frontend.onrender.com","http://localhost:5174"],
+    origin: ["https://luxycart-frontend.onrender.com","https://luxycart-admin.onrender.com"],
     credentials: true
 }))
 
